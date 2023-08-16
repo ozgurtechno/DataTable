960972f87242888c457f86a9d5d62e0584cc07af
Ozgur Techno <ozgur.technostudy@gmail.com>
2023-08-16 09:55:05 -0400

    Initial commit

---
+++ src/test/java/utilities/ParameterDriver.java
@@ -0,0 +1,56 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ParameterDriver {
+
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+    private static ThreadLocal<String> threadDriverName = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (threadDriver.get() == null) {
+
+            if (threadDriverName.get()==null){
+                threadDriverName.set("chrome");
+            }
+            Logger logger = Logger.getLogger("");
+            logger.setLevel(Level.SEVERE);
+
+            switch (threadDriverName.get()) {
+                case "firefox":
+                    threadDriver.set(new FirefoxDriver());
+                    break;
+                case "safari":
+                    threadDriver.set(new SafariDriver());
+                    break;
+                case "edge":
+                    threadDriver.set(new EdgeDriver());
+                    break;
+                default:
+                    threadDriver.set(new ChromeDriver());
+                    break;
+            }
+            threadDriver.get().manage().window().maximize();
+        }
+        return threadDriver.get();
+    }
+
+    public static void quitDriver(){
+        if (threadDriver.get()!=null){
+            threadDriver.get().quit();
+            WebDriver driver = null;
+            threadDriver.set(driver);
+        }
+    }
+
+    public static void setThreadDriverName(String browserName){
+        threadDriverName.set(browserName.toLowerCase());
+    }
+}
